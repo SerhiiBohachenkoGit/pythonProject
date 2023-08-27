065dc36ca7af44f77aca070a5121a9a0cdc305d3
serhiib <serhii.bohachenko@gmail.com>
2023-08-27 19:47:04 +0300

    fonc

---
+++ funcs/enumerate.py
@@ -0,0 +1,6 @@
+names = ['Bob', 'Alice', 'Guido']
+for index, value in enumerate(names): #  пробегает по коллекции и присваивает каждому элементу индекс начинас с 0
+    print(f'{index}: {value}')
+
+
+
---
+++ funcs/isinstance.py
@@ -0,0 +1,10 @@
+"""
+Функция isinstance() в Python используется для проверки, является ли объект экземпляром указанного класса или нет.
+Источник: https://pythonim.ru/osnovy/funktsii-isinstance-i-issubclass-v-python
+
+Синтаксис функции isinstance(): isinstance(object, classinfo)
+isinstance(object, classinfo)
+"""
+
+i = 10
+print('i is int:', isinstance(i, int))
